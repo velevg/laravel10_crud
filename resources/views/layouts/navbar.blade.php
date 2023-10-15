7150ca49374b609bdbef67c4e19aa79cf96d06f8
velevg <georgevelev.95@gmail.com>
2023-10-15 13:00:41 +0300

    laravel10_crud

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container">
+        <a href="#" class="navbar-toggler">Navbar</a>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a href="/" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item">
+                <a href="{{ route('product.index') }}" class="nav-link">Product</a>
+            </li>
+        </ul>
+    </div>
+</nav>
